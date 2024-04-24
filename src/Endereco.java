2bcf769978725927184f1634be2cccb5a04b3ce8
Rogerio Corbo <rogeriocgprest@gmail.com>
2024-04-24 14:34:29 -0300

    realizado o upload dos arquivos

---
+++ src/Endereco.java
@@ -0,0 +1,5 @@
+public record Endereco(String cep , String logradouro, String complemento,
+                       String localidade,String uf) {
+}
+
+// Classe record imutável receberá os dados da API viaCep
---
+++ src/Principal.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        ConsultaCep consultaCep = new ConsultaCep();
+        System.out.println("Digite um numero de Cep para leitura.:");
+        var cep = leitura.nextLine();
+
+        try {
+            Endereco novoEndereco = consultaCep.buscaEndereco(cep);
+            System.out.println("Arquivo Json com o endereço foi criado corretamente");
+            GeradorDeArquivo gerador = new GeradorDeArquivo();
+            gerador.salvaJson(novoEndereco);
+        } catch (RuntimeException | IOException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Finalizando a Aplicação !");
+        }
+    }
+}
